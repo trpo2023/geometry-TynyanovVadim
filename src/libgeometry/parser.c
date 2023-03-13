1057478c46b7f0f94bee535be9f9d23e0ea449a5
TynyanovVadim <vadiktynyanov@yandex.ru>
2023-03-13 09:00:35 +0700

    Fail test

--- src/libgeometry/parser.c
+++ src/libgeometry/parser.c
@@ -28,7 +28,7 @@ void count_polygon_tokens(char* line, Polygon* polygon)
 
 int get_circle(Circle* circle, char* line)
 {
-    char symbol = NULL;
+    char* symbol = NULL;
     for (int i = 0; line[i] != '\0'; i++) {
         if (line[i] == '(') {
             symbol = &(line[i]);
