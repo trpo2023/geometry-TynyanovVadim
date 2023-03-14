c2418091c6822492e66c820e583d9006426cc0bb
TynyanovVadim <vadiktynyanov@yandex.ru>
2023-03-14 09:24:08 +0700

    Add triangle and polygon perimetr func

--- src/libgeometry/parser.c
+++ src/libgeometry/parser.c
@@ -164,7 +164,7 @@ double circle_perimetr(const Circle circle)
 
 double two_points_distant(const Point point1, const Point point2)
 {
-    return sqrt(pow(point1.x - point2.x, 2) + pow(point1.y - point2.y, 2))
+    return sqrt(pow(point1.x - point2.x, 2) + pow(point1.y - point2.y, 2));
 }
 
 double figure_perimetr(const Point* cords, size_t len)
