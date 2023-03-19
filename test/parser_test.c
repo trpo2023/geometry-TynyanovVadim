f00a04562352f10f9dbefc7c661134737b128503
TynyanovVadim <vadiktynyanov@yandex.ru>
2023-03-19 11:07:10 +0700

    Add perimetr func test

--- test/parser_test.c
+++ test/parser_test.c
@@ -4,7 +4,7 @@
 #include "libgeometry/parser.h"
 #include "libgeometry/lexer.h"
 
-CTEST(surface_suite, surf_circle)
+CTEST(surface_suite, circle)
 {
     Point p = {0, 0};
     Circle circle = {p, 5};
@@ -13,7 +13,7 @@ CTEST(surface_suite, surf_circle)
     ASSERT_DBL_NEAR(expected_surface, real_surface);
 }
 
-CTEST(surface_suite, surf_polygon)
+CTEST(surface_suite, polygon)
 {
     Point p1 = {0, 0};
     Point p2 = {4, 0};
@@ -26,7 +26,7 @@ CTEST(surface_suite, surf_polygon)
     ASSERT_DBL_NEAR(expected_surface, real_surface);
 }
 
-CTEST(surface_suite, surf_triangle)
+CTEST(surface_suite, triangle)
 {
     Point p1 = {0, 0};
     Point p2 = {3, 0};
@@ -36,4 +36,38 @@ CTEST(surface_suite, surf_triangle)
     double expected_surface = 6;
     double real_surface = triangle_surface(triangle);
     ASSERT_DBL_NEAR(expected_surface, real_surface);
+}
+
+CTEST(perimetr_suite, circle)
+{
+    Point p = {0, 0};
+    Circle circle = {p, 5};
+    double expected_perimetr = (2 * M_PI * circle.radius);
+    double real_perimetr = circle_perimetr(circle);
+    ASSERT_DBL_NEAR(expected_perimetr, real_perimetr);
+}
+
+CTEST(perimetr_suite, polygon)
+{
+    Point p1 = {0, 0};
+    Point p2 = {4, 0};
+    Point p3 = {4, 4};
+    Point p4 = {0, 4};
+    Point cords[] = {p1, p2, p3, p4, p1};
+    Polygon polygon = {cords, 5};
+    double expected_perimetr = 16;
+    double real_perimetr = polygon_perimetr(polygon);
+    ASSERT_DBL_NEAR(expected_perimetr, real_perimetr);
+}
+
+CTEST(perimetr_suite, triangle)
+{
+    Point p1 = {0, 0};
+    Point p2 = {3, 0};
+    Point p3 = {3, 4};
+    Point cords[] = {p1, p2, p3, p1};
+    Triangle triangle = {cords};
+    double expected_perimetr = 12;
+    double real_perimetr = triangle_perimetr(triangle);
+    ASSERT_DBL_NEAR(expected_perimetr, real_perimetr);
 }
