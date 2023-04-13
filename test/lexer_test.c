38196975f6fe2aa904bdd7f18cbca97f1c432bef
TynyanovVadim <vadiktynyanov@yandex.ru>
2023-04-13 11:01:39 +0700

    Rewrite test for new error function

--- test/lexer_test.c
+++ test/lexer_test.c
@@ -31,9 +31,17 @@ CTEST(lexer_siute, check_brackets_true)
     char* ln2 = "triangle((1 1, 5 5, 0 0, 1 1))";
     char* ln3 = "polygon((1 1, 5 5, 0 0, 2 2, 1 1))";
 
-    ASSERT_TRUE(check_brackets(ln1, strlen(ln1)));
-    ASSERT_TRUE(check_brackets(ln2, strlen(ln2)));
-    ASSERT_TRUE(check_brackets(ln3, strlen(ln3)));
+    char* error1;
+    char* error2;
+    char* error3;
+
+    check_brackets(ln1, strlen(ln1), &error1);
+    check_brackets(ln2, strlen(ln2), &error2);
+    check_brackets(ln3, strlen(ln3), &error3);
+
+    ASSERT_NULL(error1);
+    ASSERT_NULL(error2);
+    ASSERT_NULL(error3);
 }
 
 CTEST(lexer_siute, check_brackets_false)
@@ -42,9 +50,17 @@ CTEST(lexer_siute, check_brackets_false)
     char* ln2 = "triangle(1 1, 5 5, 0 0, 1 1)";
     char* ln3 = "polygon(1 1, 5 5, 0 0, 2 2, 1 1))(";
 
-    ASSERT_FALSE(check_brackets(ln1, strlen(ln1)));
-    ASSERT_FALSE(check_brackets(ln2, strlen(ln2)));
-    ASSERT_FALSE(check_brackets(ln3, strlen(ln3)));
+    char* error1;
+    char* error2;
+    char* error3;
+
+    check_brackets(ln1, strlen(ln1), &error1);
+    check_brackets(ln2, strlen(ln2), &error2);
+    check_brackets(ln3, strlen(ln3), &error3);
+
+    ASSERT_NOT_NULL(error1);
+    ASSERT_NOT_NULL(error2);
+    ASSERT_NOT_NULL(error3);
 }
 
 CTEST(lexer_siute, unexpected_token_true)
@@ -53,9 +69,17 @@ CTEST(lexer_siute, unexpected_token_true)
     char* ln2 = "triangle((1 1, 5 5, 0 0, 1 1))";
     char* ln3 = "polygon((1 1, 5 5, 0 0, 2 2, 1 1))";
 
-    ASSERT_TRUE(check_unexpected_token(ln1, strlen(ln1)));
-    ASSERT_TRUE(check_unexpected_token(ln2, strlen(ln2)));
-    ASSERT_TRUE(check_unexpected_token(ln3, strlen(ln3)));
+    char* error1;
+    char* error2;
+    char* error3;
+
+    check_unexpected_token(ln1, strlen(ln1), &error1);
+    check_unexpected_token(ln2, strlen(ln2), &error2);
+    check_unexpected_token(ln3, strlen(ln3), &error3);
+
+    ASSERT_NULL(error1);
+    ASSERT_NULL(error2);
+    ASSERT_NULL(error3);
 }
 
 CTEST(lexer_siute, unexpected_token_false)
@@ -64,9 +88,17 @@ CTEST(lexer_siute, unexpected_token_false)
     char* ln2 = "triangle((1 1, 5 5, 0 0, 1 1)) ()()";
     char* ln3 = "polygon((1 1, 5 5, 0 0, 2 2, 1 1)) ,;";
 
-    ASSERT_FALSE(check_unexpected_token(ln1, strlen(ln1)));
-    ASSERT_FALSE(check_unexpected_token(ln2, strlen(ln2)));
-    ASSERT_FALSE(check_unexpected_token(ln3, strlen(ln3)));
+    char* error1;
+    char* error2;
+    char* error3;
+
+    check_unexpected_token(ln1, strlen(ln1), &error1);
+    check_unexpected_token(ln2, strlen(ln2), &error2);
+    check_unexpected_token(ln3, strlen(ln3), &error3);
+
+    ASSERT_NOT_NULL(error1);
+    ASSERT_NOT_NULL(error2);
+    ASSERT_NOT_NULL(error3);
 }
 
 CTEST(lexer_siute, is_argument_correct_true)
@@ -75,9 +107,17 @@ CTEST(lexer_siute, is_argument_correct_true)
     char* ln2 = "triangle((1 1, 5 5, 0 0, 1 1))";
     char* ln3 = "polygon((1 1, 5 5, 0 0, 2 2, 1 1))";
 
-    ASSERT_TRUE(is_argument_correct(ln1));
-    ASSERT_TRUE(is_argument_correct(ln2));
-    ASSERT_TRUE(is_argument_correct(ln3));
+    char* error1;
+    char* error2;
+    char* error3;
+
+    is_argument_correct(ln1, &error1);
+    is_argument_correct(ln2, &error2);
+    is_argument_correct(ln3, &error3);
+
+    ASSERT_NULL(error1);
+    ASSERT_NULL(error2);
+    ASSERT_NULL(error3);
 }
 
 CTEST(lexer_siute, is_argument_correct_false)
@@ -86,29 +126,15 @@ CTEST(lexer_siute, is_argument_correct_false)
     char* ln2 = "triangle((1 1, 5 5, 0 0, 1 1, 0 0))";
     char* ln3 = "polygon((1 1, 5 5, 1 1))";
 
-    ASSERT_FALSE(is_argument_correct(ln1));
-    ASSERT_FALSE(is_argument_correct(ln2));
-    ASSERT_FALSE(is_argument_correct(ln3));
-}
+    char* error1;
+    char* error2;
+    char* error3;
 
-CTEST(lexer_siute, is_syntax_correct_true)
-{
-    char* ln1 = "circle(0 0, 5)";
-    char* ln2 = "triangle((1 1, 5 5, 0 0, 1 1))";
-    char* ln3 = "polygon((1 1, 5 5, 0 0, 2 2, 1 1))";
+    is_argument_correct(ln1, &error1);
+    is_argument_correct(ln2, &error2);
+    is_argument_correct(ln3, &error3);
 
-    ASSERT_TRUE(is_syntax_correct(ln1, strlen(ln1)));
-    ASSERT_TRUE(is_syntax_correct(ln2, strlen(ln2)));
-    ASSERT_TRUE(is_syntax_correct(ln3, strlen(ln3)));
-}
-
-CTEST(lexer_siute, is_syntax_correct_false)
-{
-    char* ln1 = "circle(0 d, 5)";
-    char* ln2 = "triangle((1 1, 5 5, 0 0, 1 1, 0 0))";
-    char* ln3 = "polygon((1 1, 5 5, 0 0, 1 d))";
-
-    ASSERT_FALSE(is_syntax_correct(ln1, strlen(ln1)));
-    ASSERT_FALSE(is_syntax_correct(ln2, strlen(ln2)));
-    ASSERT_FALSE(is_syntax_correct(ln3, strlen(ln3)));
+    ASSERT_NOT_NULL(error1);
+    ASSERT_NOT_NULL(error2);
+    ASSERT_NOT_NULL(error3);
 }
